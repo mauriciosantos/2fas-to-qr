9043409d3edd78fc0cacaadf770d3d1c4915c9f9
Mauricio Santos <1291187+mauriciosantos@users.noreply.github.com>
2023-11-06 22:14:06 -0500

    Initial commit

---
+++ 2fas_to_qr.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import json
+import os
+import qrcode
+import sys
+
+from pathvalidate import sanitize_filename
+
+def _eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+    sys.exit(1)
+    
+class AuthCode:
+     def __init__(self, service):
+        self.name = service['name']
+        self.secret = service['secret']
+        self.token_type = service['otp']['tokenType']
+        if self.token_type != 'TOTP':
+            raise Exception(f'Unsupported token type {self.token_type} for account {self.name}')
+        self.account = service['otp'].get('account', self.name)
+        self.otpauth = f'otpauth://totp/{self.account}?secret={self.secret}&issuer={self.name}'
+        self.qr = qrcode.make(self.otpauth)
+
+def twofas_to_qr(service):
+    return AuthCode(service)
+    
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        _eprint(f'Usage: {sys.argv[0]} <2fas_input_file> <output_directory>')
+        
+    input = sys.argv[1]
+    output = sys.argv[2]
+        
+    if not os.path.isfile(input):
+        _eprint(f'Input file does not exist: {input}')
+        
+    if not os.path.isdir(output):
+        _eprint(f'Output directory does not exist: {output}')
+
+    f = open(input)
+    data = json.load(f)
+    services = data['services'];
+
+    for idx, service in enumerate(services):
+        auth_code = twofas_to_qr(service)
+        auth_code.qr.save(f'{output}/qr_{idx}_{sanitize_filename(auth_code.name)}.png')
+        
+    print(f'Exported {len(services)} QR codes')
+    
---
+++ README.md
@@ -0,0 +1,18 @@
+# Overview
+
+Converts 2FAS authenticator backup files to QR codes that can be imported into any other authenticator app.
+
+See: <https://2fas.com/>
+
+## Usage
+
+Using the app go to Settings -> Backup -> Export -> Disable Set Password Option -> Export to File
+
+Move the file to a system with python3 and execute script:
+
+```bash
+pip install -r requirements.txt
+./2fas_to_qr.py '<2FAS Input File>' '<Output Directory>'
+```
+
+Then scan the generated codes using your authenticator app of preference. Remember to delete the 2FAS backup file as well as generated QR codes afterwards! They contain the plaintext secrets that anyone can see.
